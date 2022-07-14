d4bb0ab17cb63f0ad68579d644454c3f8a95e9ec
ivfly <iv0fly@gmail.com>
2022-07-14 12:00:32 +0200

    PreparedStatement

--- src/it/beije/ragazzi/database/BibliotecaJDBC.java
+++ src/it/beije/ragazzi/database/BibliotecaJDBC.java
@@ -2,6 +2,7 @@ package it.beije.ragazzi.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -12,8 +13,8 @@ public class BibliotecaJDBC {
 		try {
 			System.out.println("Sono nel try");
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/biblioteca?serverTimezone=CET&useSSL=false",
-					"root", "Marcellona1994!");
+//			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/biblioteca?serverTimezone=CET&useSSL=false", "root", "Marcellona1994!");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/biblioteca?serverTimezone=CET&useSSL=false", "root", "beije");
 			System.out.println("connection open ? " + !connection.isClosed());
 			Statement statement = connection.createStatement();
 		// INSERT
@@ -21,6 +22,19 @@ public class BibliotecaJDBC {
 		// VALUES ('Piero', 'Verde', '5355223532', 'l.verde@beije.it')");
 		// statement.executeUpdate("INSERT INTO utenti VALUES (null, 'Piero', 'Verde2',
 		// '5355223532', 'l.verde2@beije.it')");
+			String nome = "Piero";
+			String cognome = "Rossi";
+			String telefono = "04231442134";
+			String email = "pr3@beije.it";
+//			String query = "INSERT INTO utenti VALUES (null, '" + nome + "', '" + cognome + "', '" + telefono + "', '" + email + "')";
+//			System.out.println(query);
+			//statement.executeUpdate(query);
+			PreparedStatement insertPrepStatement = connection.prepareStatement("INSERT INTO utenti VALUES (null, ?, ?, ?, ?)");
+			insertPrepStatement.setString(1, nome);
+			insertPrepStatement.setString(2, cognome);
+			insertPrepStatement.setString(3, telefono);
+			insertPrepStatement.setString(4, email);
+			insertPrepStatement.execute();
 
 		// UPDATE
 		// statement.executeUpdate("UPDATE utenti SET telefono = '1234567' where id =
@@ -34,28 +48,28 @@ public class BibliotecaJDBC {
 //		statement.executeUpdate("insert into prestiti values (null, 1, 1987, 'Bari', '3/2/2022')");
 		
 //		ResultSet rs = statement.executeQuery("SELECT * FROM prestiti");
-			ResultSet rs = statement.executeQuery("SELECT * FROM prestiti");
+//			ResultSet rs = statement.executeQuery("SELECT * FROM prestiti");
 
-		// ResultSet rs = statement.executeQuery("SELECT nome, cognome, email FROM
-		// utenti");
-			while (rs.next()) {
+		ResultSet rs = statement.executeQuery("SELECT * FROM utenti");
+		while (rs.next()) {
 			// System.out.println("id : " + rs.getInt(1));
 			// System.out.println("nome : " + rs.getString(2));
 			// System.out.println("cognome : " + rs.getString(3));
 			// System.out.println("email : " + rs.getString(4));
 			// System.out.println("telefono : " + rs.getString(5));
 
-//			System.out.println("id : " + rs.getInt("id"));
-//			System.out.println("nome : " + rs.getString("nome"));
-//			System.out.println("cognome : " + rs.getString("cognome"));
-//			System.out.println("email : " + rs.getString("email"));
-//			System.out.println("telefono : " + rs.getString("telefono"));
-//			System.out.println("paese : " + rs.getString("paese"));
-				System.out.println("id_prestito : " + rs.getInt("id"));
-				System.out.println("id_utente : " + rs.getInt("id_utente"));
-				System.out.println("id_libro : " + rs.getInt("id_libro"));
-				System.out.println("luogo_prestito : " + rs.getString("luogo_prestito"));
-				System.out.println("data_restituzione : " + rs.getString("data_restituzione"));
+			System.out.println("id : " + rs.getInt("id"));
+			System.out.println("nome : " + rs.getString("nome"));
+			System.out.println("cognome : " + rs.getString("cognome"));
+			System.out.println("email : " + rs.getString("email"));
+			System.out.println("telefono : " + rs.getString("telefono"));
+			System.out.println("paese : " + rs.getString("paese"));
+		
+//				System.out.println("id_prestito : " + rs.getInt("id"));
+//				System.out.println("id_utente : " + rs.getInt("id_utente"));
+//				System.out.println("id_libro : " + rs.getInt("id_libro"));
+//				System.out.println("luogo_prestito : " + rs.getString("luogo_prestito"));
+//				System.out.println("data_restituzione : " + rs.getString("data_restituzione"));
 			}
 //		ResultSet rsPrestiti = statement.executeQuery("select * from prestiti");
 //		while (rsPrestiti.next()) {
